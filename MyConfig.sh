2d26593fe3e6fbb1e286681353cdcb9ebc134cdd
Goodyaoshi <liyaoshi0328@qq.com>
2021-08-22 14:40:18 +0800

    提交配置文件

---
+++ MyConfig.sh
@@ -0,0 +1,4 @@
+cd /www/wwwroot/qiandao
+wget -C https://cdn.jsdelivr.net/gh/goodyaoshi/MyConfig@master/qiandao/setup.json
+cd /www/wwwroot/NovelNotice
+wget -C https://cdn.jsdelivr.net/gh/goodyaoshi/MyConfig@master/NovelNotice/setup.json
