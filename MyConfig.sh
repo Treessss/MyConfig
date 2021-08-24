52a947e9ee35a5b767e49d3be883de133eebab99
Goodyaoshi <liyaoshi0328@qq.com>
2021-08-24 09:03:32 +0800

    修复设置错误

--- MyConfig.sh
+++ MyConfig.sh
@@ -1,4 +1,6 @@
 cd /www/wwwroot/qiandao
+rm -rf setup.json
 wget -c https://cdn.jsdelivr.net/gh/goodyaoshi/MyConfig@master/qiandao/setup.json
 cd /www/wwwroot/NovelNotice
+rm -rf setup.json
 wget -c https://cdn.jsdelivr.net/gh/goodyaoshi/MyConfig@master/NovelNotice/setup.json
