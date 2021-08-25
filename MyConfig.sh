392edf9e16495748073e6382478ae633dfe80117
Goodyaoshi <liyaoshi03328@qq.com>
2021-08-25 02:30:12 +0000

    2021-08-25 02:30:12

--- MyConfig.sh
+++ MyConfig.sh
@@ -1,15 +1,5 @@
-cd /www/wwwroot
-yes | cp /www/wwwroot/MyConfig/qiandao/setup.json /www/wwwroot/qiandao/setup.json
 cd /www/wwwroot/MyConfig
-git add NovelNotice/setup.json
-git add MyConfig.sh
-git commit -m "$(date +"%Y-%m-%d %H:%M:%S")"
-git push -u origin master
-
-cd /www/wwwroot/qiandao
-rm -rf setup.json
-wget -c https://cdn.jsdelivr.net/gh/goodyaoshi/MyConfig@master/qiandao/setup.json
-cd /www/wwwroot/NovelNotice
-rm -rf setup.json
-wget -c https://cdn.jsdelivr.net/gh/goodyaoshi/MyConfig@master/NovelNotice/setup.json
+git pull
+yes | cp /www/wwwroot/MyConfig/qiandao/setup.json /www/wwwroot/qiandao/setup.json
+yes | cp /www/wwwroot/MyConfig/NovelNotice/setup.json /www/wwwroot/NovelNotice/setup.json
 
